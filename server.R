9da2663e1db630613814cebd709159297afe881b
Oleg <oleg@oleg2.cursol.local>
2016-11-04 00:14:37 +0000

    2016-11-04 00.13

--- server.R
+++ server.R
@@ -7,8 +7,109 @@
 
 library(shiny)
 
-shinyServer(function(input, output) {
 
+shinyServer(function(input, output) {
+  
+  source("global.R", local=TRUE)
+  
+  output$client.buys.rnd <- renderUI({
+    choices <- as.character(g.ccy[g.ccy$name == input$ccy, c("base", "quote")])
+    radioButtons(inputId="client.buys", label="Client Buys", choices=choices, selected=NULL, inline=TRUE, width=NULL)
+  })
+  
+  output$notional.ccy.rnd <- renderUI({
+    choices <- as.character(g.ccy[g.ccy$name == input$ccy, c("base", "quote")])
+    radioButtons(inputId="notional.ccy", label="Notional Ccy", choices=choices, selected=NULL, inline=TRUE, width=NULL)
+  })
+  
+  # output$client.buys.rnd <- renderUI({
+  #   choices <- as.character(g.ccy[g.ccy$name == input$ccy, c("base", "quote")])
+  #   selectInput(inputId="client.buys", label="Client Buys", choices=choices)
+  # })
+  
+  # output$notional.ccy.rnd <- renderUI({
+  #   choices <- as.character(g.ccy[g.ccy$name == input$ccy, c("base", "quote")])
+  #   selectInput(inputId="notional.ccy", label="Notional Ccy", choices=choices)
+  # })
+  
+  output$expiry1.rnd <- renderUI({
+    if(as.numeric(input$strip.occurrences) <= 1)
+    {
+      return(dateInput(inputId="expiry1", label="Expiry 1", value=NULL, format="dd-M-yyyy", weekstart=1, startview="month"))
+    }
+  })
+  
+  output$notional1.rnd <- renderUI({
+    if(as.numeric(input$strip.occurrences) <= 1)
+    {
+      return(numericInput(inputId="notional1", label="Notional 1", value=NULL, min=100, max=10^7, step=0.01, width=NULL))
+    }
+  })
+  
+  output$ratio.rnd <- renderUI({
+    sp <- g.sp[g.sp$name == input$sp, ]
+    if(sp$ratio)
+      return(numericInput(inputId="ratio.amount", label="Ratio Amount", value=NULL, min=100, max=10^7, step=0.01, width=NULL))
+  })
+  
+  output$par1.ni <- renderUI({
+    sp <- g.sp[g.sp$name == input$sp, ]
+    if(sp$code == "RKIKOF")
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "b.digits")])
+      return(numericInput(inputId="init.strike", label="Initial Strike / Barrier", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    } else {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "s.digits")])
+      return(numericInput(inputId="strike", label="Strike Rate", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+  })
+  
+  output$par1.vt <- renderUI({
+    sp <- g.sp[g.sp$name == input$sp, ]
+    if(sp$code == "RKIKOF")
+    {
+      return(verbatimTextOutput(outputId="init.strike.inv"))
+    } else {
+      return(verbatimTextOutput(outputId="strike.inv"))
+    }
+  })
+  
+  
+  output$par2.ni <- renderUI({
+    sp <- g.sp[g.sp$name == input$sp, ]
+    if(sp$code == "RKIKOF")
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "s.digits")])
+      return(numericInput(inputId="post.strike", label="Post Barrier Strike", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+    if(sp$reduction)
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "s.digits")])
+      return(numericInput(inputId="reduction", label="Reduction Rate", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+    if(sp$barrier)
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "b.digits")])
+      return(numericInput(inputId="barrier", label="Barrier Rate", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+  })
+  
+  output$par3.ni <- renderUI({
+    sp <- g.sp[g.sp$name == input$sp, ]
+    if(sp$code == "RSC")
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "s.digits")])
+      return(numericInput(inputId="collar.rate", label="Collar Rate", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+    if(sp$reduction & sp$barrier)
+    {
+      v <- as.numeric(g.ccy[g.ccy$name == input$ccy, c("low", "high", "b.digits")])
+      return(numericInput(inputId="barrier", label="Barrier Rate", value=NULL, min=v[1], max=v[2], step=1/(10^v[3]), width=NULL))
+    }
+  })
+  
+  
+  
   output$distPlot <- renderPlot({
 
     # generate bins based on input$bins from ui.R
@@ -20,7 +121,10 @@ shinyServer(function(input, output) {
 
   })
   
+  
+  
   output$strike.inv <- renderText(round(1/input$strike, g.ccy$s.digits[g.ccy$name == input$ccy]))
+  output$init.strike.inv <- renderText(round(1/input$init.strike, g.ccy$b.digits[g.ccy$name == input$ccy]))
   
   
 
--- ui.R
+++ ui.R
@@ -10,22 +10,36 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("SP Hedge Calculator"),
+  titlePanel("CS Structured Products"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       
       fluidRow(
-        column(6, selectInput(inputId="sp", label="Structured Product", choices=g.sp$name, selected=NULL, width=NULL)),
-        column(3, selectInput(inputId="ccy", label="Ccy Pair", choices=g.ccy$name, selected=NULL, selectize=TRUE, width=NULL))
+        column(8, selectInput(inputId="sp", label="Structured Product", choices=g.sp$name, selected=NULL, width=NULL)),
+        column(4, selectInput(inputId="ccy", label="Ccy Pair", choices=g.ccy$name, selected=NULL, selectize=TRUE, width=NULL))
       ),
       
       fluidRow(
-        column(4,
-               numericInput(inputId="strike", label="Strike", value=NULL, min=1/(10^max(g.ccy$s.digits)), step=1/(10^max(g.ccy$s.digits)), width=NULL),
-               verbatimTextOutput(outputId="strike.inv")),
-        column(3, selectInput(inputId="ccy", label="Ccy Pair", choices=g.ccy$name, selected=NULL, selectize=TRUE, width=NULL))
+        column(4, uiOutput("client.buys.rnd")),
+        column(4, uiOutput("notional.ccy.rnd"))
+      ),
+      
+      radioButtons(inputId="strip.occurrences", label="Strip (Occurrences)", choices=1:6, selected=NULL, inline=TRUE, width=NULL),
+      
+      
+      fluidRow(
+        #column(3, uiOutput("client.buys.rnd")),
+        column(4, numericInput(inputId="notional.amount", label="Notional Amount", value=NULL, min=100, max=10^7, step=0.01, width=NULL)),
+        #column(3, uiOutput("notional.ccy.rnd")),
+        column(4, uiOutput("ratio.rnd"))
+      ),
+      
+      fluidRow(
+        column(4, uiOutput("par1.ni"), uiOutput("par1.vt")),
+        column(4, uiOutput("par2.ni"), uiOutput("par2.vt")),
+        column(4, uiOutput("par3.ni"), uiOutput("par3.vt"))
       ),
       
       
@@ -38,6 +52,22 @@ shinyUI(fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
+      
+      fluidRow(
+        column(uiOutput("expiry1.rnd"),
+               uiOutput("expiry2.rnd"),
+               uiOutput("expiry3.rnd"),
+               uiOutput("expiry4.rnd"),
+               uiOutput("expiry5.rnd"),
+               uiOutput("expiry6.rnd"), width=2),
+        column(uiOutput("notional1.rnd"),
+               uiOutput("notional2.rnd"),
+               uiOutput("notional3.rnd"),
+               uiOutput("notional4.rnd"),
+               uiOutput("notional5.rnd"),
+               uiOutput("notional6.rnd"), width=2)
+      ),
+      
       plotOutput("distPlot")
     )
   )
